51cae34496bad6abd247c72ffdc389c359f9b6b5
rahmimuaz <rahmimuaz10@gmail.com>
2025-03-18 22:27:55 +0530

    frontend deleted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,10 @@
 import React, { useState ,useContext,useEffect} from 'react';
 import Navbar from './components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Cart from './pages/Cart/Cart';
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import LoginPopup from './components/LoginPopup/LoginPopup'; 
-import ContactUsForm from './components/Inquiry/ContactUsForm'; 
 import { StoreContext } from './context/StoreContext';
-import OrderConfirmation from './pages/OrderConformation/OrderConfirmation';
-import UserOrders from './pages/UserOrders/Order'
-import UserQueries from './components/Inquiry/UserQueries';
-import EditInquiryForm from './components/Inquiry/EditInquiryForm';
 import Login from './components/Login/Login'
-
 import 'bootstrap/dist/css/bootstrap.css';
-
-import Card from './pages/Card/CardManager'
-
 import StudentManagement from "./pages/StudentManagement/StudentManagement";
 
 const App = () => {
@@ -32,21 +20,11 @@ const App = () => {
 
   return (
     <div className='app'>
-
-
       {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
-
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/card' element={<Card />} />
-        <Route path='/order' element={<PlaceOrder />} />
-        <Route path='/u-orders' element={<UserOrders />} />
-        <Route path='/order-confirmation' element={<OrderConfirmation />} />
+        <Route path='/students' element={<StudentManagement />} />
         <Route path='/login' element={<LoginPopup setShowLogin={setShowLogin} />} />
-        <Route path="/contact-us" element={<ContactUsForm />} />
-        <Route path="/queries" element={<UserQueries />} />
-        <Route path="/edit-inquiry/:id" element={<EditInquiryForm />} /> {/* Ensure correct route here */}
       </Routes>
     </div>
   );
